ee810a5f2259fb28bdc69adea52f8b72855d13eb
VimHater <24520826@gm.uit.edu.vn>
2026-03-24 09:23:16 +0700

    add .gitignore

--- build.ps1
+++
@@ -1,5 +0,0 @@
-g++ main.cpp ./rlImGui/*.cpp ./imgui/*.cpp -o ./bin/main.exe `
-    -I./imgui/ `
-    -I./raylib-mingw/include/ `
-    -L./raylib-mingw/lib/ `
-    -lraylib -lopengl32 -lgdi32 -lwinmm
