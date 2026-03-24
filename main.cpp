153a819cd632a7260b7a34e55315954f06c551d5
VimHater <24520826@gm.uit.edu.vn>
2026-03-24 15:25:50 +0700

    add CmakeLists.txt

--- main.cpp
+++ main.cpp
@@ -13,9 +13,9 @@ int main() {
     rlImGuiSetup(true);
     ImGui::GetIO().FontGlobalScale = 2.0f;
     Camera3D camera = { 0 };
-    camera.position = (Vector3){ 10.0f, 10.0f, 10.0f };
-    camera.target = (Vector3){ 0.0f, 0.0f, 0.0f };
-    camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };
+    camera.position = Vector3{ 10.0f, 10.0f, 10.0f };
+    camera.target = Vector3{ 0.0f, 0.0f, 0.0f };
+    camera.up = Vector3{0.0f, 1.0f, 0.0f };
     camera.fovy = 45.0f;
     camera.projection = CAMERA_PERSPECTIVE;
 
@@ -29,25 +29,20 @@ int main() {
     Color sphereColor = RED;
 
     while (!WindowShouldClose()) {
-        // Update
         if (!ImGui::GetIO().WantCaptureMouse) {
             UpdateCamera(&camera, CAMERA_ORBITAL);
         }
 
-        // --- DRAWING PHASE ---
-        
-        // A. Draw 3D Scene to Texture
         BeginTextureMode(screenTarget);
             ClearBackground(DARKGRAY);
             BeginMode3D(camera);
-                DrawCube((Vector3){0,0,0}, cubeSize, cubeSize, cubeSize, cubeColor);
-                DrawSphere((Vector3){0, 0, 3}, sphereRadius, sphereColor);                                     // Draw sphere
-                DrawCubeWires((Vector3){0,0,0}, cubeSize, cubeSize, cubeSize, BLACK);
+                DrawCube(Vector3{0,0,0}, cubeSize, cubeSize, cubeSize, cubeColor);
+                DrawSphere(Vector3{0, 0, 3}, sphereRadius, sphereColor);                                     // Draw sphere
+                DrawCubeWires(Vector3{0,0,0}, cubeSize, cubeSize, cubeSize, BLACK);
                 DrawGrid(10, 1.0f);
             EndMode3D();
         EndTextureMode();
 
-        // B. Draw UI to Screen
         BeginDrawing();
             ClearBackground(BLACK);
             rlImGuiBegin();
@@ -61,7 +56,7 @@ int main() {
                 ImGui::EndMainMenuBar();
             }
 
-            // Sidebar (Properties)
+            // Sidebar
             ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(10,10));
             ImGui::Begin("PropertiesCube");
                 ImGui::Text("Object: Cube");
@@ -70,7 +65,7 @@ int main() {
 
                 float colCube[3] = { cubeColor.r/255.f, cubeColor.g/255.f, cubeColor.b/255.f };
                 if (ImGui::ColorEdit3("Color", colCube)) {
-                    cubeColor = (Color){ (uint8_t)(colCube[0]*255), (uint8_t)(colCube[1]*255), (uint8_t)(colCube[2]*255), 255 };
+                    cubeColor = Color{ (uint8_t)(colCube[0]*255), (uint8_t)(colCube[1]*255), (uint8_t)(colCube[2]*255), 255 };
                 }
 
             ImGui::End();
@@ -84,12 +79,12 @@ int main() {
 
                 float colSphere[3] = { cubeColor.r/255.f, cubeColor.g/255.f, cubeColor.b/255.f };
                 if (ImGui::ColorEdit3("Color", colSphere)) {
-                    cubeColor = (Color){ (uint8_t)(colSphere[0]*255), (uint8_t)(colSphere[1]*255), (uint8_t)(colSphere[2]*255), 255 };
+                    cubeColor = Color{ (uint8_t)(colSphere[0]*255), (uint8_t)(colSphere[1]*255), (uint8_t)(colSphere[2]*255), 255 };
                 }
             ImGui::End();
             ImGui::PopStyleVar();
 
-            // Viewport (The 3D Scene)
+            // Viewport
             ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(10,10));
             ImGui::Begin("Viewport");
                 // Calculate aspect ratio fit
@@ -103,7 +98,6 @@ int main() {
         EndDrawing();
     }
 
-    // Cleanup
     UnloadRenderTexture(screenTarget);
     rlImGuiShutdown();
     CloseWindow();
